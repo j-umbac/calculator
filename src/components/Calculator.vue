6a409f821e497d75fcfafb23c5a51c509dddaac8
Joshua Umbac <joshuarheyumbac14@gmail.com>
2021-06-17 15:15:16 +0800

    Init

---
+++ src/components/Calculator.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="Calculator">
+    <h1>Vue Calculator</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.calculator {
+  grid-template-columns: repeat(4, 1fr);
+  grid-auto-rows: minmax(50px, auto);
+}
+</style>
